42cb1638f311971d96a14171b1c2242af13f5bc7
AhmedRaza0609 <ahmedrazamusani@gmail.com>
2020-08-22 16:52:00 +0500

    Cryptography

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 
-dep_type = st.selectbox('Select Decription Method', ['None', 'Atbash', 'Caeser', 'A2Z'])
+dep_type = st.selectbox('Select Decription Method', ['None', 'Atbash', 'Caeser', 'A2Z', 'Atbash + A2Z'])
 if dep_type == 'Atbash':
     txt = st.text_input('Choose the text you want to encrypt/decrypt (Both return the same answer in Atbash)')
     answer = Atbash(txt)
@@ -36,3 +36,16 @@ if dep_type == 'A2Z':
         answer = A2Z(txt, method)
         if st.button('Submit'):
             st.success(answer)
+
+if dep_type == 'Atbash + A2Z':
+    method = st.selectbox('Choose whether you want to encrypt or decrypt a message:', ['None', 'Encrypt', 'Decrypt'])
+    if method == 'Encrypt':
+        txt = st.text_input('Choose the text you want to encrypt')
+        returnvalue = Atbash(txt)
+        answer = A2Z(returnvalue, method)
+    elif method == 'Decrypt':
+        txt = st.text_input('Choose the text you want to decrypt (Space Each Number for eg: 10 5 4, not 1054), Use ? in between two words, if you want to. ? should also be spaced out like 1 ? 2')
+        returnvalue = A2Z(txt, method)
+        answer = Atbash(returnvalue)
+    if st.button('Submit'):
+        st.success(answer)
